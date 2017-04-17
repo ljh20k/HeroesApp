e48a58934d04e97158e27035226ed1433fce0736
ljh20k <wngns123>
2017-04-17 21:49:36 +1000

    Third Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    [BrowserAnimationsModule],
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
