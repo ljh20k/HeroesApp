ee527e58a55878c137efc457d158dfb67ad4e229
ljh20k <wngns123>
2017-04-25 11:46:05 +1000

    13th Commit: Routing completed

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService } from './hero.service';
@@ -12,12 +12,13 @@ import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
 })
 
 export class HeroDetailComponent implements OnInit{
 
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private heroService: HeroService,
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 //import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: [ './heroes.component.css' ],
   //providers: [HeroService] //To teach the injector how to make a HeroService
 })
 
@@ -14,7 +15,7 @@ export class HeroesComponent implements OnInit{
   heroes : Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private router: Router) {}
 
   getHeroes(): void {
   this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -26,5 +27,9 @@ export class HeroesComponent implements OnInit{
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
